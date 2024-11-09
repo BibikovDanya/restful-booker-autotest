14ddb1ed46b4f8e095620e323d0b4b4ddb14abda
d.bibikov <bibikov.danya@gmail.com>
2024-11-09 22:08:05 +0300

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
+    testImplementation("com.fasterxml.jackson.core", "jackson-databind", "2.18.1")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
