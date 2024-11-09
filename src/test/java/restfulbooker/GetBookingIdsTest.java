def6049e7df00464e430a2f817ea1eaaec0c6673
d.bibikov <bibikov.danya@gmail.com>
2024-11-10 00:57:49 +0300

    added spec and GetBookingIdsTest

---
+++ src/test/java/restfulbooker/GetBookingIdsTest.java
@@ -0,0 +1,42 @@
+package restfulbooker;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+import static io.restassured.RestAssured.*;
+
+public class GetBookingIdsTest {
+    private static final String baseUrl = "http://localhost:3001";
+//    private static final String baseUrl = "http://restful-booker.herokuapp.com";
+
+    @BeforeAll
+    public static void initSpecification(){
+       Specification.installSpecification(Specification.requestSpec(baseUrl + "/booking"));
+       Specification.installSpecification(Specification.responseSpecOK200());
+    }
+
+    @Test
+    public void getBookingIdTest() {
+        Response response = given()
+                .when()
+                .get()
+                .then().log().all()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertNotNull(response);
+
+        JsonPath jsonPath = response.jsonPath();
+        List<Integer> bookingIds = jsonPath.getList("bookingid");
+        System.out.println(bookingIds.toString());
+        Assertions.assertTrue(bookingIds.stream().allMatch(Objects::nonNull),
+                "Все идентификаторы книг должны быть целыми числами");
+
+    }
+}
