8a035a14c939776857f17880a0def9b1877e7c47
d.bibikov <bibikov.danya@gmail.com>
2024-12-02 23:45:22 +0300

    override equals && hashCode && toString  and refactoring

--- src/test/java/restfulbooker/BookData.java
+++ src/test/java/restfulbooker/BookData.java
@@ -1,75 +1,144 @@
 package restfulbooker;
 
-public class BookData {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    private String firstname;
-    private String lastname;
-    private Integer totalprice;
-    private boolean depositpaid;
-    private Bookingdates bookingdates;
-    private String additionalneeds;
+import java.util.Objects;
+
+public class BookData {
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    @JsonProperty("totalprice")
+    private Integer totalPrice;
+
+    @JsonProperty("depositpaid")
+    private boolean depositPaid;
+
+    @JsonProperty("bookingdates")
+    private BookingDates bookingDates;
+
+    @JsonProperty("additionalneeds")
+    private String additionalNeeds;
 
     public BookData() {
     }
 
-    public BookData(String firstname, String lastname, Integer totalprice, boolean depositpaid, Bookingdates bookingdates, String additionalneeds) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.totalprice = totalprice;
-        this.depositpaid = depositpaid;
-        this.bookingdates = bookingdates;
-        this.additionalneeds = additionalneeds;
+    public BookData(String firstName, String lastname, Integer totalPrice, boolean depositPaid, BookingDates bookingDates, String additionalNeeds) {
+        this.firstName = firstName;
+        this.lastName = lastname;
+        this.totalPrice = totalPrice;
+        this.depositPaid = depositPaid;
+        this.bookingDates = bookingDates;
+        this.additionalNeeds = additionalNeeds;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public Integer getTotalprice() {
-        return totalprice;
+    public Integer getTotalPrice() {
+        return totalPrice;
     }
 
-    public boolean isDepositpaid() {
-        return depositpaid;
+    public boolean isDepositPaid() {
+        return depositPaid;
     }
 
-    public Bookingdates getBookingdates() {
-        return bookingdates;
+    public BookingDates getBookingDates() {
+        return bookingDates;
     }
 
-    public String getAdditionalneeds() {
-        return additionalneeds;
+    public String getAdditionalNeeds() {
+        return additionalNeeds;
+    }
+
+    @Override
+    public String toString() {
+        return "BookData{" +
+                "firstName='" + this.firstName + '\'' +
+                ", lastName='" + this.lastName + '\'' +
+                ", totalPrice=" + this.totalPrice +
+                ", depositPaid=" + this.depositPaid +
+                ", bookingDates=" + this.bookingDates +
+                ", additionalNeeds='" + this.additionalNeeds + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BookData that = (BookData) obj;
+        return depositPaid == that.depositPaid &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(totalPrice, that.totalPrice) &&
+                Objects.equals(bookingDates, that.bookingDates) &&
+                Objects.equals(additionalNeeds, that.additionalNeeds);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, totalPrice, depositPaid, bookingDates, additionalNeeds);
     }
 }
-class Bookingdates{
-    private String checkin;
-    private String checkout;
 
-    public Bookingdates() {
+class BookingDates {
+    @JsonProperty("checkin")
+    private String checkIn;
+    @JsonProperty("checkout")
+    private String checkOut;
+
+    public BookingDates() {
     }
 
-    public Bookingdates(String checkin, String checkout) {
-        this.checkin = checkin;
-        this.checkout = checkout;
+    public BookingDates(String checkIn, String checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
-    public String getCheckin() {
-        return checkin;
+    public String getCheckIn() {
+        return checkIn;
     }
 
-    public String getCheckout() {
-        return checkout;
+    public String getCheckOut() {
+        return checkOut;
     }
 
-    public void setCheckin(String checkin) {
-        this.checkin = checkin;
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
     }
 
-    public void setCheckout(String checkout) {
-        this.checkout = checkout;
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingDates{" +
+                "checkOut='" + this.checkOut + '\'' +
+                ", checkIn='" + this.checkIn + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BookingDates that = (BookingDates) obj;
+        return Objects.equals(checkIn, that.checkIn) && Objects.equals(checkOut, that.checkOut);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(checkIn, checkOut);
     }
 }
