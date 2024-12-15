04250f8e9c5fe18d283debd7d98205841208dc39
d.bibikov <bibikov.danya@gmail.com>
2024-12-15 17:14:43 +0300

    refactoring GetBookingIdsTest

--- src/test/java/restfulbooker/tests/GetBookingIdsTest.java
+++ src/test/java/restfulbooker/tests/GetBookingIdsTest.java
@@ -1,34 +1,19 @@
-package restfulbooker;
+package restfulbooker.tests;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Objects;
 
-import static io.restassured.RestAssured.*;
-
 public class GetBookingIdsTest extends BaseBookingTest {
-    private static final String baseUrl = "http://localhost:3001";
-//    private static final String baseUrl = "http://restful-booker.herokuapp.com";
-
-//    @BeforeAll
-//    public static void initSpecification(){
-//       Specification.installSpecification(Specification.requestSpec(baseUrl + "/booking"));
-//       Specification.installSpecification(Specification.responseSpecOK200());
-//    }
 
     @Test
     public void getBookingIdTest() {
-//        Response response2 = given()
-//                .when()
-//                .get()
-//                .then().log().all()
-//                .extract().response();
-       Response response = getAllBooking();
+
+        Response response = getAllBooking();
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertNotNull(response);
