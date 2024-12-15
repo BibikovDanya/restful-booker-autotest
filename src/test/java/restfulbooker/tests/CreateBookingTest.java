1f6ebcaf2e2323b133511ea579be3dfc6b515f8f
d.bibikov <bibikov.danya@gmail.com>
2024-12-15 17:18:41 +0300

    update createBookTest

--- src/test/java/restfulbooker/tests/CreateBookingTest.java
+++ src/test/java/restfulbooker/tests/CreateBookingTest.java
@@ -10,8 +10,7 @@ public class CreateBookingTest extends BaseBookingTest {
 
     @Test
     public void createBookTest() {
-        BookingDates bookingDates = new BookingDates("2022-01-01", "2022-01-02");
-        BookData book = new BookData("Jim", "Brown", 111, true, bookingDates, "Breakfast");
+        BookData book = new BookData("Jim", "Brown", 111, true, "2022-01-01", "2022-01-02", "Breakfast");
         Response response = createBooking(book);
 
         BookData createdBook = response.jsonPath().getObject("booking", BookData.class);
