6ee1080086d32491b0f2238fec0d4ae099ab7995
d.bibikov <d.bibikov@crpt.ru>
2025-03-13 19:10:22 +0300

    migration to testcontainers

--- src/test/java/restfulbooker/tests/BaseBookingTest.java
+++ src/test/java/restfulbooker/tests/BaseBookingTest.java
@@ -1,7 +1,10 @@
 package restfulbooker.tests;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 import restfulbooker.api.requests.bookings.BookingRequest;
 import restfulbooker.helpers.BookingHelpers;
 import restfulbooker.utils.Specification;
@@ -10,17 +13,32 @@ public class BaseBookingTest {
     protected BookingRequest bookingRequest;
     protected BookingHelpers bookingHelpers;
 
-    //TODO config env
-    private static final String baseUrl = "http://localhost:3001";
-//    private static final String baseUrl = "http://restful-booker.herokuapp.com";
+
+    private static GenericContainer<?> container;
+    private static final String IMAGE_NAME = "restful-booker:latest";
+    private static final int PORT = 3001;
+    private static String baseUrl;
 
 
     @BeforeAll
     public static void initSpecification() {
+        container = new GenericContainer<>(DockerImageName.parse(IMAGE_NAME)).withExposedPorts(PORT);
+        container.start();
+
+        baseUrl = "http://" + container.getHost() + ":" + container.getMappedPort(PORT);
+
         Specification.installSpecification(Specification.requestSpec(baseUrl));
         Specification.installSpecification(Specification.responseSpecOK200());
 
     }
+
+    @AfterAll
+    public static void tearDown() {
+        if (container != null) {
+            container.stop();
+        }
+    }
+
     @BeforeEach
     public void setUp() {
         bookingRequest = new BookingRequest();
